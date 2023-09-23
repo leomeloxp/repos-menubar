0b64678312bed42cee6857ac7bcd0311bcd3066a
Leo Melo <pgp@leomeloxp.dev>
2023-09-23 18:22:03 +0100

    feat: add git logo as background in app view

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,38 @@
   import SettingsView from "./views/SettingsView.svelte";
 </script>
 
-<main class="container">
-  {#if $route === "settings"}
-    <SettingsView />
-  {:else}
-    <MainView />
-  {/if}
-</main>
+<div class="wrapper">
+  <main class="container">
+    {#if $route === "settings"}
+      <SettingsView />
+    {:else}
+      <MainView />
+    {/if}
+  </main>
+</div>
 
 <style>
+  .wrapper {
+    position: relative;
+  }
+
+  .wrapper:before {
+    content: " ";
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.2;
+    background-image: url("/git-icon-logo.svg");
+    background-repeat: no-repeat;
+    background-position: bottom right;
+    background-size: 50%;
+  }
+
   .container {
+    position: relative;
     padding: 32px 16px;
     display: flex;
     flex-direction: column;
